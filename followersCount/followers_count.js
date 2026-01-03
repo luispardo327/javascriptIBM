6dfdada254c1b78eaeddec947dd4c4ac46f721f2
luispardo327 <luispardo327@gmail.com>
2026-01-03 11:22:51 -0500

    conteo de seguidores Proyecto guiado

---
+++ followersCount/followers_count.js
@@ -0,0 +1,18 @@
+let count = 0;
+
+function increaseCount(){
+    count++;
+    displayCount();
+    checkCountValue();
+}
+
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML=count;
+}
+function checkCountValue(){
+    if (count ===10){
+        alert("¡Tu publicacion de Instagram gano 10 seguidores! ¡Felcidades!");
+    }else if (count === 20){
+        alert("¡Tu publicacion de Instagram gano 20 seguidores! ¡Sigue así!");
+    }
+}
