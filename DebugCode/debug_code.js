cf41a4a3b0d0230d7211d2e5ef8f457b243c89e7
luispardo327 <luispardo327@gmail.com>
2026-02-26 20:45:14 -0500

    tarea de crear funciones de otras operaciones y unirla a la funcion principal

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -1,3 +1,5 @@
+let currentValue ;
+let result;
 function performOperation(){
     //Obtener la entrada del usuario desde los campos de entrada
     let num1 = parseInt(document.getElementById('input1').value);
@@ -5,13 +7,32 @@ function performOperation(){
     // Verificar si las entradas son números válidos
     if (!isNaN(num1) && !isNaN(num2)){
        //Realiza la operación
-       let result = multiply(num1, num2);
-       
-       //mostrar el resultado
+
+       switch (currentValue) {
+  case ('addition'):
+    result = addition(num1, num2);
+    break;
+  case ('subtraction'):
+    result = subtraction(num1, num2);
+    break;
+   case ('split'):
+    if (num2 ===0){
+        result = 'error no se puede dividir por cero';
+    }else{
+        result = split(num1, num2);
+    }
+    break;
+    case ('multiply'):
+    result = multiply(num1, num2);
+    break;
+  default:
+    displayResult('por favor ingrese una operacion');
+}      //mostrar el resultado
        displayResult(result);
     }else{
         displayResult('por favor ingrese un numero valido');
     }
+    return;
 }
 
 function multiply(a, b){
@@ -27,4 +48,22 @@ function displayResult(result){
     // Mostrar el resultado en el elemento de párrafo
     const resultElement = document.getElementById('result');
     resultElement.textContent = `El Resultado es: ${result}`;
+}
+
+function addition (a,b){
+    debugger;
+
+    return a +b;
+}
+
+function  subtraction(a,b){
+    debugger;
+
+    return a - b;
+}
+
+function split (a,b){
+    debugger;
+
+    return a / b;
 }
